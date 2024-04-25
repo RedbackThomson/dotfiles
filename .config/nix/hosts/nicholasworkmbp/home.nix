181e1176232a16660bfbb2139c2b70abbcfd94da
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2024-04-25 11:09:06 -0700

    Add kube switch configuration to work laptop

--- .config/nix/hosts/nicholasworkmbp/home.nix
+++ .config/nix/hosts/nicholasworkmbp/home.nix
@@ -1,7 +1,4 @@
 {
-  programs.ssh = {
-    enable = true;
-    extraConfig = ''
-    '';
-  };
+  xdg.configFile."kubeconfigs/ops.yaml".source = ./kubeswitch/ops.yaml;
+  home.file.".kube/switch-config.yaml".source = ./kubeswitch/switch-config.yaml;
 }
