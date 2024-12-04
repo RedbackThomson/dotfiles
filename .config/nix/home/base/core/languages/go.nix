748977dbfb8243da686f3a4547502883eacfef92
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2024-12-04 13:48:50 -0800

    Add Go zshrc env vars to Go config

--- .config/nix/home/base/core/languages/go.nix
+++ .config/nix/home/base/core/languages/go.nix
@@ -18,4 +18,11 @@
     package = pkgs-unstable.go_1_22;
     goPrivate = [ "github.com/upbound" ];
   };
+
+  programs.zsh.initExtra = ''
+    export GOROOT=$(go env GOROOT)
+    export GOPATH=$(go env GOPATH)
+    export GOBIN=$GOPATH/bin
+    export PATH="$GOBIN:$PATH"
+  '';
 }
