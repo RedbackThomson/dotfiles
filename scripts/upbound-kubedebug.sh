bd2de4d04616e873c4c4217e02db300f127680ae
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2025-03-24 19:12:30 -0700

    Upgrade upbound kubedebug

--- scripts/upbound-kubedebug.sh
+++ scripts/upbound-kubedebug.sh
@@ -4,7 +4,7 @@ install_kubedebug() {
     local namespace
     namespace="$1"
 
-    kubectl -n "$namespace" apply -f https://storage.googleapis.com/mcpdebug/kubedebug.yaml
+    kubectl -n "$namespace" apply -f https://storage.googleapis.com/mcpdebug/v2/kubedebug.yaml
     sleep 5
     kubectl -n "$namespace" patch networkpolicy allow-ingress-to-vcluster --type=json -p=\[\{\"op\":\ \"add\",\ \"path\":\ \"/spec/ingress/0/from/-\",\ \"value\":\ \{\"podSelector\":\{\"matchLabels\":\{\"app\":\"tenant-kubedebug\"\}\}\}\}\]
     kubectl -n "$namespace" exec -it deploy/tenant-kubedebug -- bash
