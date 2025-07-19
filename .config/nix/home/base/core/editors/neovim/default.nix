898024a51b8a7f0ea1df0141c63fb7717c52ce13
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2025-07-19 12:42:17 -0700

    Basic neovim config

--- .config/nix/home/base/core/editors/neovim/default.nix
+++ .config/nix/home/base/core/editors/neovim/default.nix
@@ -20,6 +20,12 @@
         };
       };
       extraPackages = [pkgs.nodejs pkgs.gopls]; # For CoC
+      extraLuaConfig = ''
+        vim.g.mapleader = " "
+        vim.g.maplocalleader = " "
+
+        require("keys")
+      '';
       extraConfig = ''
         set hidden
         set autoindent
@@ -29,6 +35,7 @@
         set noerrorbells
         set number
         set numberwidth=4
+        set rnu
         set nowrap
         set showcmd
         set scrolloff=3
@@ -49,10 +56,22 @@
 
             lua << END
               require('nvim-treesitter.configs').setup {
+                incremental_selection = {
+                  enable = true,
+                  keymaps = {
+                    init_selection = "gnn", -- set to `false` to disable one of the mappings
+                    node_incremental = "grn",
+                    scope_incremental = "grc",
+                    node_decremental = "grm",
+                  },
+                },
                 indent = {
                   enable = true
                 }
               }
+
+              vim.wo.foldmethod = 'expr'
+              vim.wo.foldexpr = 'nvim_treesitter#foldexpr()'
             END
           '';
         }
@@ -97,7 +116,50 @@
             END
           '';
         }
+        {
+          plugin = trouble-nvim;
+          config = ''
+            lua << END
+              require("trouble").setup()
+            END
+          '';
+        }
+        {
+          plugin = hop-nvim;
+          config = ''
+            lua << END
+              require("hop").setup()
+
+              local map = require("keys").map
+              map("n", "HH", ":HopWord<cr>")
+              map("n", "HF", ":HopPattern<cr>")
+              map("n", "HL", ":HopLineStart<cr>")
+            END
+          '';
+        }
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "nvim-web-devicons";
+          version = "0422a19d9aa3aad2c7e5cca167e5407b13407a9d";
+          src = pkgs.fetchFromGitHub {
+            owner = "nvim-tree";
+            repo = "nvim-web-devicons";
+            rev = "0422a19d9aa3aad2c7e5cca167e5407b13407a9d";
+            sha256 = "0qnigbhyxwc5mwyb6az3f9bh8609rvllkg34x6qp5vffaslfbhwd";
+          };
+        })
+        (pkgs.vimUtils.buildVimPlugin {
+          pname = "vim-be-good";
+          version = "0ae3de14eb8efc6effe7704b5e46495e91931cc5";
+          src = pkgs.fetchFromGitHub {
+            owner = "ThePrimeagen";
+            repo = "vim-be-good";
+            rev = "0ae3de14eb8efc6effe7704b5e46495e91931cc5";
+            sha256 = "1kqb4ljlwypilq23y5d83njh08z5w8mlk323qcqp521jvmbmkcya";
+          };
+        })
       ];
     };
   };
+
+    xdg.configFile."nvim/lua".source = ./lua;
 }
