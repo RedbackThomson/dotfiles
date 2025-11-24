57fec2169dd4df9c90aa78dbed588a39750d0694
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2025-11-24 11:36:56 -0800

    Add stripe CLI

--- .config/nix/hosts/nicholasworkmbp/home.nix
+++ .config/nix/hosts/nicholasworkmbp/home.nix
@@ -1,4 +1,8 @@
-{
+{ pkgs, ...}: {
   xdg.configFile."kubeconfigs/ops.yaml".source = ./kubeswitch/ops.yaml;
   home.file.".kube/switch-config.yaml".source = ./kubeswitch/switch-config.yaml;
+
+  home.packages = with pkgs; [
+    stripe-cli
+  ];
 }
