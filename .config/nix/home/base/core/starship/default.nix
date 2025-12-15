37d823ecaac1dbe5e796641a03cd0fb3e8610fc6
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2025-12-15 13:20:55 -0800

    Add jj to starship

--- .config/nix/home/base/core/starship/default.nix
+++ .config/nix/home/base/core/starship/default.nix
@@ -46,6 +46,47 @@
         ];
       };
 
+      custom = {
+        jj = {
+          command = 
+            ''
+              jj log --revisions @ --no-graph --ignore-working-copy --color always --limit 1 --template '
+                separate(" ",
+                  change_id.shortest(4),
+                  bookmarks,
+                  "|",
+                  concat(
+                    if(conflict, "💥"),
+                    if(divergent, "🚧"),
+                    if(hidden, "👻"),
+                    if(immutable, "🔒"),
+                  ),
+                  raw_escape_sequence("\x1b[1;32m") ++ if(empty, "(empty)"),
+                  raw_escape_sequence("\x1b[1;32m") ++ coalesce(
+                    truncate_end(29, description.first_line(), "…"),
+                    "(no description set)",
+                  ) ++ raw_escape_sequence("\x1b[0m"),
+                )
+              '
+            '';
+          when = "jj --ignore-working-copy root";
+          symbol = " ";
+        };
+      };
+
+      # optionally disable git modules
+      git_state.disabled = true;
+      git_commit.disabled = true;
+      git_metrics.disabled = true;
+      git_branch.disabled = true;
+
+      # re-enable git_branch as long as we're not in a jj repo
+      custom.git_branch = {
+        when = true;
+        command = "jj root >/dev/null 2>&1 || starship module git_branch";
+        description = "Only show git_branch if we're not in a jj repo";
+      };
+
       format = lib.concatStrings [
         " " # added
         "$username"
@@ -61,6 +102,7 @@
         "$git_state"
         "$git_metrics"
         "$git_status"
+        "\${custom.jj}"
         "$hg_branch"
         "$docker_context"
         "$package"
@@ -102,6 +144,7 @@
         "$vagrant"
         "$zig"
         "$nix_shell"
+        "$nats" # Added
         "$conda"
         "$spack"
         "$memory_usage"
@@ -111,7 +154,7 @@
         "$azure"
         "$env_var"
         "$crystal"
-        "$custom"
+        # "$custom" # Removed - use individual custom modules instead
         "$sudo"
         "$cmd_duration"
         "$line_break"
