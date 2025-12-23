6570b113e7da5f2b235c157b5e30533751acf188
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2025-12-23 12:54:04 -0800

    Add fzf-tab

--- .config/nix/home/base/core/shells/default.nix
+++ .config/nix/home/base/core/shells/default.nix
@@ -43,6 +43,10 @@ in {
           sha256 = "0za4aiwwrlawnia4f29msk822rj9bgcygw6a8a6iikiwzjjz0g91";
         };
       }
+      {
+        name = "fzf-tab";
+        src = "${pkgs.zsh-fzf-tab}/share/fzf-tab";
+      }
     ];
     localVariables = {
       ZSH_AUTOSUGGEST_STRATEGY = ["history" "completion"];
