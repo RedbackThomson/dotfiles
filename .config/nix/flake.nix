fe86edbd58fb9bf147144aac33da90adaf1607f6
Nicholas Thomson <RedbackThomson@users.noreply.github.com>
2025-12-23 08:54:05 -0800

    Add yazi

--- .config/nix/flake.nix
+++ .config/nix/flake.nix
@@ -4,17 +4,20 @@
   outputs = inputs: import ./outputs inputs;
 
   nixConfig = {
-    extra-trusted-substituters = ["https://cache.flox.dev"];
-    extra-trusted-public-keys = ["flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="];
+    extra-trusted-substituters = [
+      "https://cache.flox.dev"
+      "https://yazi.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "flox-cache-public-1:7F4OyH7ZCnFhcze3fJdfyXYLQw/aV7GEed86nQ7IsOs="
+      "yazi.cachix.org-1:Dcdz63NZKfvUCbDGngQDAZq6kOroIrFoyO064uvLh8k="
+    ];
   };
 
   inputs = {
-    nixpkgs = {
-      url = "github:nixos/nixpkgs/release-25.05";
-    };
-    nixpkgs-unstable = {
-      url = "github:nixos/nixpkgs/nixpkgs-unstable";
-    };
+    nixpkgs.url = "github:nixos/nixpkgs/release-25.05";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     nix-darwin = {
       url = "github:lnl7/nix-darwin/nix-darwin-25.05";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -27,13 +30,8 @@
       url = "github:cachix/pre-commit-hooks.nix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    nur-ryan4yin = {
-      url = "github:ryan4yin/nur-packages";
-      # inputs.nixpkgs.follows = "nixpkgs";
-    };
-    _1password-shell = {
-      url = "github:1Password/shell-plugins";
-    };
+    nur-ryan4yin.url = "github:ryan4yin/nur-packages";
+    _1password-shell.url = "github:1Password/shell-plugins";
     haumea = {
       url = "github:nix-community/haumea/v0.2.2";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -42,11 +40,8 @@
       url = "github:dj95/zjstatus";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    darwin-custom-icons = {
-      url = "github:ryanccn/nix-darwin-custom-icons";
-    };
-    flox = {
-      url = "github:flox/flox/latest";
-    };
+    darwin-custom-icons.url = "github:ryanccn/nix-darwin-custom-icons";
+    flox.url = "github:flox/flox/latest";
+    yazi.url = "github:sxyazi/yazi";
   };
 }
--- .config/nix/home/base/core/essential.nix
+++ .config/nix/home/base/core/essential.nix
@@ -4,131 +4,151 @@
   pkgs,
   attic,
   nur-ryan4yin,
+  yazi,
   ...
 }: {
   config = lib.mkIf config.myconfig.core.essential.enable {
     home.packages = with pkgs; [
-    # Misc
-    gnupg
-    gnumake
+      # Misc
+      gnupg
+      gnumake
 
-    # Morden cli tools, replacement of grep/sed/...
+      # Morden cli tools, replacement of grep/sed/...
 
-    # Interactively filter its input using fuzzy searching, not limit to filenames.
-    fzf
-    # search for files by name, faster than find
-    fd
-    # search for files by its content, replacement of grep
-    (ripgrep.override {withPCRE2 = true;})
+      # Interactively filter its input using fuzzy searching, not limit to filenames.
+      fzf
+      # search for files by name, faster than find
+      fd
+      # search for files by its content, replacement of grep
+      (ripgrep.override {withPCRE2 = true;})
 
-    # A fast and polyglot tool for code searching, linting, rewriting at large scale
-    # supported languages: only some mainstream languages currently(do not support nix/nginx/yaml/toml/...)
-    ast-grep
+      # A fast and polyglot tool for code searching, linting, rewriting at large scale
+      # supported languages: only some mainstream languages currently(do not support nix/nginx/yaml/toml/...)
+      ast-grep
 
-    yq-go # yaml processer https://github.com/mikefarah/yq
-    delta # A viewer for git and diff output
-    neofetch # A fast, highly customisable system info script
+      yq-go # yaml processer https://github.com/mikefarah/yq
+      delta # A viewer for git and diff output
+      neofetch # A fast, highly customisable system info script
 
-    # nix related
-    #
-    # it provides the command `nom` works just like `nix
-    # with more details log output
-    nix-output-monitor
-    hydra-check # check hydra(nix's build farm) for the build status of a package
-    nix-index # A small utility to index nix store paths
-    nix-init # generate nix derivation from url
-    # https://github.com/nix-community/nix-melt
-    nix-melt # A TUI flake.lock viewer
-    # https://github.com/utdemir/nix-tree
-    nix-tree # A TUI to visualize the dependency graph of a nix derivation
-  ];
+      # nix related
+      #
+      # it provides the command `nom` works just like `nix
+      # with more details log output
+      nix-output-monitor
+      hydra-check # check hydra(nix's build farm) for the build status of a package
+      nix-index # A small utility to index nix store paths
+      nix-init # generate nix derivation from url
+      # https://github.com/nix-community/nix-melt
+      nix-melt # A TUI flake.lock viewer
+      # https://github.com/utdemir/nix-tree
+      nix-tree # A TUI to visualize the dependency graph of a nix derivation
+    ];
 
-  programs = {
-    # A modern replacement for ‘ls’
-    # useful in bash/zsh prompt, not in nushell.
-    eza = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-      git = true;
-      icons = "auto";
-    };
-
-    # a cat(1) clone with syntax highlighting and Git integration.
-    bat = {
-      enable = true;
-      config = {
-        pager = "less -FR";
-        theme = "catppuccin-frappe";
+    programs = {
+      # A modern replacement for ‘ls’
+      # useful in bash/zsh prompt, not in nushell.
+      eza = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        git = true;
+        icons = "auto";
       };
-      themes = {
-        # https://raw.githubusercontent.com/catppuccin/bat/main/themes/Catppuccin%20Frappe.tmTheme
-        catppuccin-frappe = {
-          src = nur-ryan4yin.packages.${pkgs.system}.catppuccin-bat;
-          file = "Catppuccin-frappe.tmTheme";
+
+      # a cat(1) clone with syntax highlighting and Git integration.
+      bat = {
+        enable = true;
+        config = {
+          pager = "less -FR";
+          theme = "catppuccin-frappe";
+        };
+        themes = {
+          # https://raw.githubusercontent.com/catppuccin/bat/main/themes/Catppuccin%20Frappe.tmTheme
+          catppuccin-frappe = {
+            src = nur-ryan4yin.packages.${pkgs.system}.catppuccin-bat;
+            file = "Catppuccin-frappe.tmTheme";
+          };
         };
       };
-    };
 
-    # A command-line fuzzy finder
-    fzf = {
-      enable = true;
-      # https://github.com/catppuccin/fzf
-      # catppuccin-frappe
-      colors = {
-        "bg+" = "#414559";
-        "bg" = "#303446";
-        "spinner" = "#f2d5cf";
-        "hl" = "#e78284";
-        "fg" = "#c6d0f5";
-        "header" = "#e78284";
-        "info" = "#ca9ee6";
-        "pointer" = "#f2d5cf";
-        "marker" = "#f2d5cf";
-        "fg+" = "#c6d0f5";
-        "prompt" = "#ca9ee6";
-        "hl+" = "#e78284";
+      # A command-line fuzzy finder
+      fzf = {
+        enable = true;
+        # https://github.com/catppuccin/fzf
+        # catppuccin-frappe
+        colors = {
+          "bg+" = "#414559";
+          "bg" = "#303446";
+          "spinner" = "#f2d5cf";
+          "hl" = "#e78284";
+          "fg" = "#c6d0f5";
+          "header" = "#e78284";
+          "info" = "#ca9ee6";
+          "pointer" = "#f2d5cf";
+          "marker" = "#f2d5cf";
+          "fg+" = "#c6d0f5";
+          "prompt" = "#ca9ee6";
+          "hl+" = "#e78284";
+        };
       };
-    };
 
-    # zoxide is a smarter cd command, inspired by z and autojump.
-    # It remembers which directories you use most frequently,
-    # so you can "jump" to them in just a few keystrokes.
-    # zoxide works on all major shells.
-    #
-    #   z foo              # cd into highest ranked directory matching foo
-    #   z foo bar          # cd into highest ranked directory matching foo and bar
-    #   z foo /            # cd into a subdirectory starting with foo
-    #
-    #   z ~/foo            # z also works like a regular cd command
-    #   z foo/             # cd into relative path
-    #   z ..               # cd one level up
-    #   z -                # cd into previous directory
-    #
-    #   zi foo             # cd with interactive selection (using fzf)
-    #
-    #   z foo<SPACE><TAB>  # show interactive completions (zoxide v0.8.0+, bash 4.4+/fish/zsh only)
-    zoxide = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-      enableNushellIntegration = true;
-    };
-
-    # Atuin replaces your existing shell history with a SQLite database,
-    # and records additional context for your commands.
-    # Additionally, it provides optional and fully encrypted
-    # synchronisation of your history between machines, via an Atuin server.
-    atuin = {
-      enable = true;
-      enableBashIntegration = true;
-      enableZshIntegration = true;
-      enableNushellIntegration = true;
-      settings = {
-        style = "compact";
-        filter_mode_shell_up_key_binding = "session";
+      # zoxide is a smarter cd command, inspired by z and autojump.
+      # It remembers which directories you use most frequently,
+      # so you can "jump" to them in just a few keystrokes.
+      # zoxide works on all major shells.
+      #
+      #   z foo              # cd into highest ranked directory matching foo
+      #   z foo bar          # cd into highest ranked directory matching foo and bar
+      #   z foo /            # cd into a subdirectory starting with foo
+      #
+      #   z ~/foo            # z also works like a regular cd command
+      #   z foo/             # cd into relative path
+      #   z ..               # cd one level up
+      #   z -                # cd into previous directory
+      #
+      #   zi foo             # cd with interactive selection (using fzf)
+      #
+      #   z foo<SPACE><TAB>  # show interactive completions (zoxide v0.8.0+, bash 4.4+/fish/zsh only)
+      zoxide = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        enableNushellIntegration = true;
       };
+
+      # Atuin replaces your existing shell history with a SQLite database,
+      # and records additional context for your commands.
+      # Additionally, it provides optional and fully encrypted
+      # synchronisation of your history between machines, via an Atuin server.
+      atuin = {
+        enable = true;
+        enableBashIntegration = true;
+        enableZshIntegration = true;
+        enableNushellIntegration = true;
+        settings = {
+          style = "compact";
+          filter_mode_shell_up_key_binding = "session";
+        };
+      };
+
+      yazi = {
+        enable = true;
+        
+        package = yazi.packages.${pkgs.system}.default.override {
+          _7zz = pkgs._7zz-rar;  # Support for RAR extraction
+        };
+      };
+
+      # Alias `y` to `yazi`
+      zsh.initContent = ''
+        function y() {
+          local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+          yazi "$@" --cwd-file="$tmp"
+          IFS= read -r -d ''' cwd < "$tmp"
+          [ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+          rm -f -- "$tmp"
+        }
+      '';
     };
-  };
   };
 }
